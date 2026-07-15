0653200e2c37d0a29325f7a6e31b01fdf4a6692c
uzername <ivanpost777@gmail.com>
2026-07-16 00:26:20 +0300

    daily changes. You can now move around map

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -38,14 +38,32 @@ export class Game extends Scene
             'walls'
         );
 
-        const groundLayer = this.map.createLayer(
+        this.groundLayer = this.map.createLayer(
             'BaseFloorLayer',
             floorTiles
         );
-        const wallLayer = this.map.createLayer(
+        this.wallLayer = this.map.createLayer(
             'BaseWallsLayer',
             wallTiles
         );
+        this.wallPassableLayer2 = this.map.createLayer(
+            'BaseWallsPassableLayer2',
+            wallTiles
+        );
+        this.wallPassableLayer = this.map.createLayer(
+            'BaseWallsPassableLayer',
+            wallTiles
+        );                
+        this.floorConstructLayer = this.map.createLayer(
+            'ConstructFloorsLayer',
+            floorTiles
+        );
+        this.wallConstructLayer = this.map.createLayer(
+            'ConstructWallsLayer',
+            wallTiles
+        );
+        this.spawnPoints = this.map.filterObjects("ImportantPoints", obj => obj.name.startsWith( "playerspawn") );
+        this.playerXpixel = this.spawnPoints[0].x; this.playerYpixel = this.spawnPoints[0].y;
 
         this.player = new Player(this, this.playerXpixel + UtilClass.SPRITEWIDTH / 2, this.playerYpixel + UtilClass.SPRITEHEIGHT / 2, "engineer_sprites");
         this.initCamera();
@@ -53,8 +71,20 @@ export class Game extends Scene
     }
 
     checkIsWall(xPixel, yPixel) {
-        // TODO port from PHASERJS_project
-        return false;
+        var tileWall = this.wallLayer.getTileAtWorldXY(xPixel, yPixel, true, this.cameras.main);
+        var tileWall2 = this.wallConstructLayer.getTileAtWorldXY(xPixel, yPixel, true, this.cameras.main);
+        if ((tileWall != null) && (tileWall.index != -1) &&
+            ('collides' in tileWall.properties) &&
+            (tileWall.properties.collides == true) ||
+            (tileWall2 != null) && (tileWall2.index != -1) &&
+            ('collides' in tileWall2.properties) &&
+            (tileWall2.properties.collides == true)
+
+        ) {
+            return true;
+        } else {
+            return false;
+        }
     }
     checkIsStoryNPC(xPixel, yPixel) {
         // TODO port from PHASERJS_project
@@ -66,6 +96,7 @@ export class Game extends Scene
     }
 
     initCamera() {
+        /*
         this.physics.world.setBounds(
             0,
             0,
@@ -78,6 +109,7 @@ export class Game extends Scene
             this.map.widthInPixels,
             this.map.heightInPixels
         );
+        */
         this.cameras.main.startFollow(this.player, true, 1, 1);
     }
 
